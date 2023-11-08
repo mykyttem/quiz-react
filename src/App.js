7e21da9d94f620561fcbec78a93686de58bf7277
chamois1 <nrabicenko5@gmail.com>
2023-11-08 18:17:01 +0200

    Added: pages auth

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import SignUp from './sign_up';
+import SignIn from './sign_in';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">      
+          <nav>
+            <ul className="navbar">
+              <li className="nav-item">
+                <a href="/" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="/about" className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="/services" className="nav-link">Services</a>
+              </li>
+              <li className="nav-item">
+                <a href="/contact" className="nav-link">Contact</a>
+              </li>
+
+              <li className="nav-item">
+                <a href="/sign-up" className="nav-link">Sign Up</a>
+              </li>
+
+              <li className="nav-item">
+                <a href="/sign-in" className="nav-link">Sign In</a>
+              </li>
+            </ul>
+
+         
+            <Route path='/sign-up' component={ SignUp }/>
+            <Route path='/sign-in' component={ SignIn }/>
+          </nav>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+export default App;
